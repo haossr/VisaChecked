6e6da483703854918c7d9814c9a2adffdd81fefa
Hao SHENG <hao@landing.ai>
2019-04-09 06:46:50 -0700

    plots added

--- visualize/visualizer.py
+++ visualize/visualizer.py
@@ -7,15 +7,19 @@ class Visualizer:
         self.raw = raw
         self.auto_open = auto_open
 
+        self.MONTH_TYPE_COLUMN = "visa_type"
         self.MONTH_TYPE_BAR_FILE = "plots/month-type-bar.html"
 
-    def plot_month_type_bar(self):
+        self.MONTH_CONSULATE_COLUMN = "visa_consulate"
+        self.MONTH_CONSULATE_BAR_FILE = "plots/month-consulate-bar.html"
+
+    def plot_month_bar(self, category="visa_type", filename="plots/month-type-bar.html"):
         # copy the raw data
         df = self.raw.copy()
 
         # clean up the data
-        df['month'] = [ymd[:7] for ymd in df.check_start]
-        count = df.groupby(["month", "visa_type"], squeeze=True).count()['__url'].unstack()
+        df['month'] = [str(ymd)[:7] for ymd in df.check_start]
+        count = df.groupby(["month", category], squeeze=True).count()['__url'].unstack()
         count = count.fillna(0)
 
         # generate the visualization output
@@ -34,4 +38,10 @@ class Visualizer:
 
         fig = Figure(data=data, layout=_layout)
 
-        py.plot(fig, filename=self.MONTH_TYPE_BAR_FILE, auto_open=self.auto_open)
+        py.plot(fig, filename=filename, auto_open=self.auto_open)
+
+    def plot_month_type_bar(self):
+        self.plot_month_bar(self.MONTH_TYPE_COLUMN, self.MONTH_TYPE_BAR_FILE)
+
+    def plot_month_consulate_bar(self):
+        self.plot_month_bar(self.MONTH_CONSULATE_COLUMN, self.MONTH_CONSULATE_BAR_FILE)
